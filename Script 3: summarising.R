b1650329fe85b723dbf6e648bf891f7c481504cf
SethMusker <sethmusker@gmail.com>
2025-02-28 13:41:51 +0200

    start script 3
    compare table(), count(), and group_by(...) %>% summarise(n=n())

--- Script 3: summarising.R
+++ Script 3: summarising.R
@@ -9,7 +9,7 @@ data("penguins",package="palmerpenguins")
 
 print(penguins_raw)
 summary(penguins_raw)
-view(penguins_raw)
+View(penguins_raw)
 
 print(penguins)
 summary(penguins)
@@ -21,4 +21,40 @@ summary(penguins)
 ## c. Check if there are any repeat measurements
 ## d. Summarise the measurements by species, sex, etc, 
 ##    (Using mean, sd, etc.)
+## e. Remake `penguins` from `penguins_raw`
 
+
+# Load libraries ----------------------------------------------------------
+
+library(tidyverse)
+library(tidylog)
+
+# a. ----------------------------------------------------------------------
+
+# We can do this with base::table() (i.e., "base R", i.e., not tidyverse)
+table(penguins$species, penguins$island)
+# Alternatively:
+with(penguins, table(species, island))
+
+# OR we can do this using dplyr::count()
+
+penguins %>% 
+  count(species,island)
+# The output is a little less aesthetically pleasing, but much tidier
+
+# count() is a shortcut for:
+penguins %>% 
+  group_by(species,island) %>% 
+  summarise(n=n())
+
+
+# b. ----------------------------------------------------------------------
+# We want to add sex to distinguish groups
+
+# table() no longer looks all that pleasing, as we have a 3-dimensional array
+with(penguins, table(species, island, sex))
+# We also lose the NAs
+
+# Compare to count()
+penguins %>% 
+  count(species,island,sex)
